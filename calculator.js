a9b3bba54676a77856e28bcd4b27370d6d253cec
xestia <30466745+xestia@users.noreply.github.com>
2017-07-27 14:12:10 +0100

    Create calculator.js
    Main file for calculator

---
+++ calculator.js
@@ -0,0 +1,41 @@
+<!Doctype html>
+ <html>
+  <head>
+   <title>Calculator<title>
+   </head>
+   <body>
+    <style>
+     body {
+      background-color: #000000
+      }
+     </style>
+    </body>
+    <center><br>
+    <h1><b><font-color=''white''style= ''font-size:70>Javascript Calculator</font></b></h1>
+    <hr size=20 color=''white''>
+    <div style = ''width= 261px: background:''fff8dc''>
+    <form name =''calculator''
+    <input name=''display'' placeholder=''0'' style= ''width=254px; height=60px; text-align:right; font-size:30; border-radius:8px; margin:3px''/>
+    <br>
+    <input type=''button'' value=''7'' onClick''document.Calculator.display.value'' style=''width:60px; height=60px; font-size=30; border-radius:8px; margin: 3px''/>
+    <input type=''button'' value=''8'' onClick''document.Calculator.display.value'' style=''width:60px; height=60px; font-size=30; border-radius:8px''/>
+    <input type=''button'' value=''9'' onClick''document.Calculator.display.value'' style=''width:60px; height=60px; font-size=30; border-radius:8px''/>
+    <input type=''button'' value=''+'' onClick''btnplus()'' style=''width:60px; height=60px; font-size=30; border-radius:8px''/>
+    <br>
+     <input type=''button'' value=''4'' onClick''document.Calculator.display.value'' style=''width:60px; height=60px; font-size=30; border-radius:8px; margin: 3px''/>
+    <input type=''button'' value=''5'' onClick''document.Calculator.display.value'' style=''width:60px; height=60px; font-size=30; border-radius:8px''/>
+    <input type=''button'' value=''6'' onClick''document.Calculator.display.value'' style=''width:60px; height=60px; font-size=30; border-radius:8px''/>
+    <input type=''button'' value=''*'' onClick''btnplus()'' style=''width:60px; height=60px; font-size=30; border-radius:8px''/>
+    <br>
+    <input type=''button'' value=''1'' onClick''document.Calculator.display.value'' style=''width:60px; height=60px; font-size=30; border-radius:8px; margin: 3px''/>
+    <input type=''button'' value=''2'' onClick''document.Calculator.display.value'' style=''width:60px; height=60px; font-size=30; border-radius:8px''/>
+    <input type=''button'' value=''3'' onClick''document.Calculator.display.value'' style=''width:60px; height=60px; font-size=30; border-radius:8px''/>
+    <input type=''button'' value=''-'' onClick''btnplus()'' style=''width:60px; height=60px; font-size=30; border-radius:8px''/>
+    <br>
+    <input type=''button'' value=''0'' onClick''document.Calculator.display.value'' style=''width:60px; height=60px; font-size=30; border-radius:8px; margin: 3px''/>
+    <input type=''button'' value=''%'' onClick''document.Calculator.display.value'' style=''width:60px; height=60px; font-size=30; border-radius:8px''/>
+    <input type=''button'' value=''.'' onClick''document.Calculator.display.value'' style=''width:60px; height=60px; font-size=30; border-radius:8px''/>
+    <input type=''button'' value=''/'' onClick''btnplus()'' style=''width:60px; height=60px; font-size=30; border-radius:8px''/>
+    <br>
+    
+     
